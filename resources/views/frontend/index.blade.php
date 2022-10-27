ce2a932f7793a0e874339741aecf0fccd2f3bb1b
hassantaghzouti <hassan.taghzo@gmail.com>
2022-10-27 13:02:52 +0100

    version 1

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -74,7 +74,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="section-title">
-                        <h2>Trending Item meriam zouina</h2>
+                        <h2>Trending Item</h2>
                     </div>
                 </div>
             </div>
