6ec7c388c143e1b67f22a10809eedef10405f0d0
hassantaghzouti <hassan.taghzo@gmail.com>
2022-10-28 08:55:57 +0100

    new changes

--- resources/views/frontend/pages/product-grids.blade.php
+++ resources/views/frontend/pages/product-grids.blade.php
@@ -10,8 +10,8 @@
                 <div class="col-12">
                     <div class="bread-inner">
                         <ul class="bread-list">
-                            <li><a href="index1.html">Home<i class="ti-arrow-right"></i></a></li>
-                            <li class="active"><a href="blog-single.html">Shop Grid</a></li>
+                            <li><a href="{{route('home')}}">Home<i class="ti-arrow-right"></i></a></li>
+                            <li class="active"><a href="{{route('product-grids')}}">Shop Grid</a></li>
                         </ul>
                     </div>
                 </div>
